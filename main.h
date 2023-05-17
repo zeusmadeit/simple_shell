47bb3509fa2efba211973c09471db1e507796d1e
Abraham <codexapp44@gmail.com>
2023-05-17 06:41:02 -0700

    first commit

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/** header include section*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <termios.h>
+
+/* Definitions and Function Prototypes*/
+#define LIMIT 256
+#define MAXLINE 1024
+
+#endif
